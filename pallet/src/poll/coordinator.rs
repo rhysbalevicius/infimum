09eaccf6f658e4fbed92aae1b474be74aa812415
Rhys Balevicius <development@rhys.tech>
2024-06-16 01:53:00 -0500

    Add poll coord

---
+++ pallet/src/poll/coordinator.rs
@@ -0,0 +1,39 @@
+use frame_support::pallet_prelude::*;
+
+use crate::poll::{PollId};
+
+/// A zk verification key.
+#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct VerifyKey
+{
+    pub alpha_g1: Vec<u8>,
+    pub beta_g2: Vec<u8>,
+    pub gamma_g2: Vec<u8>,
+    pub delta_g2: Vec<u8>,
+    pub gamma_abc_g1: Vec<Vec<u8>>,
+}
+
+/// Coordinator storage definition.
+#[derive(Clone, Encode, Decode, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct Coordinator
+{
+    /// The coordinators public key.
+    pub public_key: PublicKey,
+
+    /// The coordinators verify key.
+    pub verify_key: VerifyKey,
+
+    /// The coordinators most recent poll (may be active).
+    pub last_poll: Option<PollId>
+}
+
+/// A public key used to facillitate secret sharing between participants and coordinators.
+#[derive(Clone, Copy, Encode, Decode, Eq, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct PublicKey 
+{
+    /// A 256-bit x-coordinate of the public key.
+    pub x: [u8; 32],
+
+    /// A 256-bit y-coordinate of the public key.
+    pub y: [u8; 32]
+}
