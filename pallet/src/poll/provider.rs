53110a307e468a5daba93f302c315f861d30a734
Rhys Balevicius <development@rhys.tech>
2024-07-14 04:28:00 -0500

    Registration tree merge populates initial commitment

--- pallet/src/poll/provider.rs
+++ pallet/src/poll/provider.rs
@@ -1,25 +1,30 @@
 use frame_support::pallet_prelude::*;
 use sp_runtime::traits::SaturatedConversion;
+use ark_bn254::{Fr};
+use ark_ff::{PrimeField, BigInteger};
+use crate::hash::{Poseidon, PoseidonHasher};
 use crate::poll::{
     Poll, 
     PublicKey,
+    PollInteractionData,
     AmortizedIncrementalMerkleTree, 
-    MerkleTreeError
+    MerkleTreeError,
+    zeroes::EMPTY_BALLOT_ROOT
 };
 
 pub trait PollProvider<T: crate::Config>: Sized
 {
-    // fn register_participant(
-    //     self, 
-    //     public_key: PublicKey, 
-    //     timestamp: u64
-    // ) -> Result<(u32, Self), MerkleTreeError>;
+    fn register_participant(
+        self, 
+        public_key: PublicKey, 
+        timestamp: u64
+    ) -> Result<(u32, Self), MerkleTreeError>;
 
-    // fn consume_interaction(
-    //     self,
-    //     public_key: PublicKey,
-    //     data: PollInteractionData
-    // ) -> Result<(u32, Self), MerkleTreeError>;
+    fn consume_interaction(
+        self,
+        public_key: PublicKey,
+        data: PollInteractionData
+    ) -> Result<(u32, Self), MerkleTreeError>;
 
     fn merge_registrations(self) -> Result<Self, MerkleTreeError>;
 
@@ -46,58 +51,6 @@ pub trait PollProvider<T: crate::Config>: Sized
 
 impl<T: crate::Config> PollProvider<T> for Poll<T>
 {
-    // fn register_participant(
-    //     mut self, 
-    //     public_key: PublicKey,
-    //     timestamp: u64
-    // ) -> Result<(u32, Self), MerkleTreeError>;
-    // {
-    //     // uint256[4] memory plaintext;
-    //     // plaintext[0] = _stateLeaf.pubKey.x;
-    //     // plaintext[1] = _stateLeaf.pubKey.y;
-    //     // plaintext[2] = _stateLeaf.voiceCreditBalance;
-    //     // plaintext[3] = _stateLeaf.timestamp;
-    //     // ciphertext = hash4(plaintext);
-
-    //     // let data = sponge::hash(&vec![
-    //     //     BlsScalar::from_raw(public_key.x),
-    //     //     BlsScalar::from_raw(public_key.y),
-    //     //     BlsScalar::from(timestamp)
-    //     // ]);
-    //     self.state.registrations = self.state.registrations.insert(leaf);
-    //     (self.state.registrations.count, self)
-    // }
-
-    // fn consume_interaction(
-    //     mut self, 
-    //     public_key: PublicKey,
-    //     data: PollInteractionData
-    // ) -> Result<(u32, Self), MerkleTreeError>;
-    // {
-    //     // let arity: usize = self.config.tree_arity.into();
-    //     // let mut data = data.map(|x| BlsScalar::from_raw(x)).to_vec();
-    //     // data.push(BlsScalar::from_raw(public_key.x));
-    //     // data.push(BlsScalar::from_raw(public_key.y));
-        
-    //     // self.state.interactions = self.state.interactions.insert(arity, sponge::hash(&data));
-    //     // (self.state.interactions.count, self)
-
-    //     // uint256[5] memory n;
-    //     // n[0] = _message.data[0];
-    //     // n[1] = _message.data[1];
-    //     // n[2] = _message.data[2];
-    //     // n[3] = _message.data[3];
-    //     // n[4] = _message.data[4];
-    
-    //     // uint256[5] memory m;
-    //     // m[0] = _message.data[5];
-    //     // m[1] = _message.data[6];
-    //     // m[2] = _message.data[7];
-    //     // m[3] = _message.data[8];
-    //     // m[4] = _message.data[9];
-    
-    //     // msgHash = hash4([hash5(n), hash5(m), _encPubKey.x, _encPubKey.y]);
-    // }
 
     fn merge_registrations(
         mut self
@@ -105,11 +58,20 @@ impl<T: crate::Config> PollProvider<T> for Poll<T>
     {
         self.state.registrations = self.state.registrations.merge()?;
 
-        // uint256[3] memory sb;
-        // sb[0] = _mergedStateRoot;
-        // sb[1] = emptyBallotRoot;
-        // sb[2] = uint256(0);
-        // currentSbCommitment = hash3(sb);
+        let Some(root) = self.state.registrations.root else { Err(MerkleTreeError::MergeFailed)? };
+        let Some(mut hasher) = Poseidon::<Fr>::new_circom(3).ok() else { Err(MerkleTreeError::HashFailed)? };
+
+        let inputs: Vec<Fr> = Vec::from([ root, EMPTY_BALLOT_ROOT, [0u8;32] ])
+            .iter()
+            .map(|bytes| Fr::from_be_bytes_mod_order(bytes))
+            .collect();
+
+        let Some(result) = hasher.hash(&inputs).ok() else { Err(MerkleTreeError::HashFailed)? };
+        let bytes = result.into_bigint().to_bytes_be();
+        let mut commitment = [0u8; 32];
+        commitment[..bytes.len()].copy_from_slice(&bytes);
+
+        self.state.commitment = (0, commitment);
 
         Ok(self)
     }
