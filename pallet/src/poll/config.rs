004c0e6cc82ec09b9a8da56a80f993949cb68e80
Rhys Balevicius <development@rhys.tech>
2024-09-01 10:57:00 -0500

    Add max_interactions to poll config

--- pallet/src/poll/config.rs
+++ pallet/src/poll/config.rs
@@ -15,6 +15,9 @@ pub struct PollConfiguration<T: crate::Config>
     /// The maximum number of participants permitted.
     pub max_registrations: u32,
 
+    /// The maximum number of interactions permitted.
+    pub max_interactions: u32,
+
     /// The subtree depth to process per commitment.
     pub process_subtree_depth: u32,
 
